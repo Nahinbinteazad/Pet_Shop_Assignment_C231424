1bfcb548d5be80a2aaa9aceea89fc7b5a81677f9
Nahinbinteazad <azadnahinbinte@gmail.com>
2025-04-04 11:54:13 +0600

    3rd commmit

--- main.js
+++ main.js
@@ -1,37 +1,3 @@
-// document.getElementById("dog").addEventListener("click", function () {
-//     let dogSection = document.getElementById("dog_section");
-
-//     if (dogSection.classList.contains("hidden")) {
-//         dogSection.classList.remove("hidden");
-//         dogSection.classList.add("block"); 
-//     } else {
-//         dogSection.classList.add("hidden"); 
-//     }
-// });
-
-// document.getElementById("bird").addEventListener("click", function () {
-//     let dogSection = document.getElementById("bird_section");
-
-//     if (dogSection.classList.contains("hidden")) {
-//         dogSection.classList.remove("hidden");
-//         dogSection.classList.add("block"); 
-        
-//     } else {
-//         dogSection.classList.add("hidden"); 
-//     }
-// });
-
-// document.getElementById("cat").addEventListener("click", function () {
-//     let dogSection = document.getElementById("cat_section");
-
-//     if (dogSection.classList.contains("hidden")) {
-//         dogSection.classList.remove("hidden");
-//         dogSection.classList.add("block"); 
-//     } else {
-//         dogSection.classList.add("hidden"); 
-//     }
-// });
-
 const sections = ["dog_section", "bird_section", "cat_section", "turtle_section", "hamster_section", "rabbit_section"];
 
 function toggleSection(sectionId) {
